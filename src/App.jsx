8532d06e188b2ddf44152f4d5639f5d120b2ce60
Mohit Kumar <mohitkumar@Mohits-MacBook-Air.local>
2025-03-29 00:20:45 +0530

    first Hello world commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import User from "./User";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>Mohit</p>
+      <User />
+    </div>
+  );
+};
+export default App;
---
+++ src/User.jsx
@@ -0,0 +1,9 @@
+function User(){
+    return (
+        <div>
+            <h1>User</h1>
+            <p>This is the user page.</p>
+        </div>
+    );
+}
+export default User;
