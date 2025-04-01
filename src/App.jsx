086ea1a0b8e4295f28611eece445b64971120a8d
Mohit Kumar <mohitkumar@Mohits-MacBook-Air.local>
2025-04-01 14:44:21 +0530

    JSX Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
-import User from './User.jsx';
+// function App() {
+//   const name = "Mohit Decodes";
+//   return(
+//     <div>
+//       <h1>Welcome to React {name}</h1>
+//       <p>Mohit Decodes {1+2}</p>
+//       <button onClick={()=>alert("Clicked")}>Click Me</button>
+//     </div>
+//   )
+// }
+// export default App;
+
+import { createElement } from "react";
 
 function App() {
-  return(
-    <div>
-      <h1>Welcome to React {abc()}</h1>
-      <p>Mohit Decodes</p>
-      <UserOne />
-      <User />
-    </div>
-  )
-}
-
-const UserOne = () => {
-  return(
-    <div>
-      <h1>User</h1>
-      <p>Mohit Decodes One</p>
-    </div>
-  )
-}
-
-function abc(){
-  return(
-    <div>
-      <h1>abc</h1>
-      <p>Mohit Decodes Two</p>
-    </div>
+  // return(
+  //   <h1 className="txt">Welcome to Mohit</h1>
+  // )
+  //Convert above code without JSX
+  return createElement(
+    'h1',
+    {className: 'txt'},
+    'Welcome to Mohit'
   )
 }
 export default App;
--- src/User.jsx
+++ src/User.jsx
@@ -1,9 +1,20 @@
 function User(){
     return (
         <div>
-            <h1>User</h1>
-            <p>User page.</p>
+            <h1>User Component</h1>
+            <p>This is the user component.</p>
         </div>
     );
 }
 export default User;
+
+export function Profile(){
+    return (
+        <div>
+            <h1>Profile Component</h1>
+            <p>This is the Profile component.</p>
+        </div>
+    );
+}
+
+export const UserName = "Mohit";
